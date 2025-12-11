a27d1606f5a5f986de93eb5b4fdf4f6ec314c309
soogiee <231131011@mhs.stiki.ac.id>
2025-12-11 13:27:06 +0700

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
             </div>
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-300 dark:border-neutral-700">
+            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-400 dark:border-neutral-700">
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
             </div>
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
